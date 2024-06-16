bbd20b9003f092b55d6b196b4cb19b3216d98f8e
lucas-tex <lucas86.tex@gmail.com>
2024-06-16 09:00:34 +0900

    cont1

--- update_csv.py
+++ update_csv.py
@@ -1,6 +1,8 @@
 import csv
 
-with open("Employee_Salaries.csv", "r") as arquivo:
-    arquivo_csv = csv.reader(arquivo, delimiter=",")
+with open('Employee_Salaries.csv', 'r') as arquivo:
+    arquivo_csv = csv.reader(arquivo, delimiter=',')
+
+# Ler os dados do arquivo
     for linha in arquivo_csv:
         print(linha)
